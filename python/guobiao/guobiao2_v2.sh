4bce50e1d96eec6034652d6d43356d94270759ca
primitivenen <wanchen.uiuc@gmail.com>
2018-11-16 11:07:53 -0800

    Update guobiao2_v2.sh

--- python/guobiao/guobiao2_v2.sh
+++ python/guobiao/guobiao2_v2.sh
@@ -5,6 +5,10 @@ endday=`date --date="today" +%Y%m%d`
 startday=`date --date="10 days ago" +%Y%m%d`
 d=$startday
 while [[ "$d" -le $endday ]]; do
+  returncode=`hdfs dfs -test -d hdfs://namenode:8020/data/guobiao/csv/d=$d`
+  if [[ "$returncode" -eq 1 ]]; then
+    continue
+  fi
   f=insert_$d.hql
   echo "USE guobiao_tsp_tbls; " >> $f
   echo "INSERT OVERWRITE TABLE guobiao_raw_orc " >> $f
