37f3ba68d1bb0708c15b2e7d7be8a1dbf2b5c371
primitivenen <wanchen.uiuc@gmail.com>
2018-12-03 13:46:25 -0800

    Update ag2.sh

--- python/ag/ag2.sh
+++ python/ag/ag2.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 cd /home/wchen/dsa/
-python ag_data.py |& tee -a /home/wchen/dsa/ag_log 
-endday=`date --date="today" +%Y%m%d`
-startday=`date --date="$days days ago" +%Y%m%d`
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+log_file=/home/wchen/dsa/ag_vehicle_raw_$current_time.log   
+python ag_data.py > $log_file 
+#python ag_data.py |& tee -a /home/wchen/dsa/ag_log 
+num_err=$(grep 'Traceback (most recent call last)' $log_file -c)
+if [ "$num_err" -ge 1 ]; then
+    mailx -s "Process failed. Check $log_file" wchen@gacrndusa.com < ${log_file}
+    echo "Process failed. Check $log_file" >&2
+    exit 1
+fi
+dir=`hdfs dfs -ls -R /data/ag/csv/ | grep "^d" | sort -k6,7 | tail -1 | tr -s ' ' | cut -d' ' -f8`
+endday=${dir:15:8}
+#endday=`date --date="today" +%Y%m%d`
+last_date=`grep \. log.txt | tail -1`
+startday=$(date -d "$last_date" +"%Y%m%d")
 d=$startday
+echo "Starting populating data for $d"
 while [ "$d" -le "$endday" ]; do
   hdfs dfs -test -d hdfs://namenode:8020/data/ag/csv/d=$d
   returncode=$?
@@ -25,5 +38,6 @@ while [ "$d" -le "$endday" ]; do
   d=$(date --date="$d + 1 day" +%Y%m%d)
   rm /home/wchen/dsa/$f
 done
-
+echo "Process done." 
+mailx -s "Process Succeed. Check $log_file" wchen@gacrndusa.com < ${log_file}  
 
